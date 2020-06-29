5074bc4bea752c3a62cab79fbbc2987ee0bad49b
Light Yakami <keepdoing999gmail.com>
2020-06-30 01:06:35 +0530

    Added view and template for detailed blog post and configured urls

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 
@@ -8,6 +8,12 @@ def post_list(request):
     posts = Post.objects.filter(
         published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_details.html', {'post': post})
+
 
 #posts = Post.objects.filter( Published_date_lte = timezone.now() ).order_by('published_date')
 # Post.objects.filter(published_date_lte=timezone.now()).order._by('published_date')
